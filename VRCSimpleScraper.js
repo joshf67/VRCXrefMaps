7b9d2d3588e82161422bcc6cdb9773212262ee84
joshf67 <iownall99@live.co.uk>
2023-05-13 17:56:43 +0100

    Update VRCSimpleScraper.js

--- VRCSimpleScraper.js
+++ VRCSimpleScraper.js
@@ -7,7 +7,7 @@ document.querySelector("#docusaurus_skipToContent_fallback > div > main > div > 
             let titleParsed = title.innerText.replace(/[\u200B-\u200D\uFEFF]/g, '');
             let parsedClassName = classname.replace(/^(.*((static class)|(class)|(enum)))/gi, "").replace(/[\u200B-\u200D\uFEFF ]/g, '');
                 
-            result += `- uid: ${parsedClassName}\n  name: ${titleParsed}\n  name.vb:\n  href: https://udonsharp.docs.vrchat.com/vrchat-api/#${titleParsed.toLowerCase().replace(" ", "-")}\n  commentId: T:${parsedClassName}\n  isSpec:\n fullName: ${parsedClassName}\n  fullName.vb:\n  nameWithType: ${parsedClassName}\n  nameWithType.vb:\n  IsValid: true\n` 
+            result += `- uid: ${parsedClassName}\n  name: ${titleParsed}\n  name.vb:\n  href: https://udonsharp.docs.vrchat.com/vrchat-api/#${titleParsed.toLowerCase().replace(" ", "-")}\n  commentId: T:${parsedClassName}\n  isSpec:\n  fullName: ${parsedClassName}\n  fullName.vb:\n  nameWithType: ${parsedClassName}\n  nameWithType.vb:\n  IsValid: true\n` 
         })
     }
 });
